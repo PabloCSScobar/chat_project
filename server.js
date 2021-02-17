a2a07daedb9c2d6e7c62626c694161a1ce806d23
pawel.gajo <gpawel1997@gmail.com>
2021-02-17 15:59:03 +0100

    dodanie serwera node

---
+++ server.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+
+
+const app = express();
+const server = http.createServer(app);
+
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
